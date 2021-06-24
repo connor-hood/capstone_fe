ecb05001ff09e9b20a01a3e7f37c702d86525412
connor-hood <connor_hood@yahoo.com>
2021-06-24 15:16:38 -0500

    got box drawn around main body and navbar left of body box

--- umlapp/src/components/Home/home.jsx
+++ umlapp/src/components/Home/home.jsx
@@ -4,7 +4,7 @@ import './home.css'
 function Home() {
     return(
         <div className="headwrap">
-            <h2>This app is designed with the music enthusiasts in mind. Search any song below and find the music video and tablature side by side for all your favorite songs!</h2>
+            <h4>This app is designed with the music enthusiasts in mind. Search any song below and find the music video and tablature side by side for all your favorite songs!</h4>
             <form>
             <label for="search"></label>
             <input type="text" id="search" name="search" placeholder="Search..."/>
--- umlapp/src/components/NavBar/navBar.jsx
+++ umlapp/src/components/NavBar/navBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './navBar.css'
 
-function NavBar(props) {
+function NavBar() {
     return (
-        <div>
-            <h5>Home</h5>
-            <h5>My Library</h5>
-            <h5>Saved Playlists</h5>
+        <div className="sidenav">
+            <p>Home</p>
+            <p>My Library</p>
+            <p>Saved Playlists</p>
             <ul>
                 <li><a href="/" >Highest Rated</a></li>
                 <li><a href="/">Custom 1</a></li>
