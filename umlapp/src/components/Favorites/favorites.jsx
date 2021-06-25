4d02ff953c12f55e295d88379b213849aecaa1ee
connor-hood <connor_hood@yahoo.com>
2021-06-25 13:14:16 -0500

    finished favorites component

---
+++ umlapp/src/components/Favorites/favorites.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Playlist from '../Playlist/playlist';
+
+function Favorites() {
+    return (
+        <div>
+            <h3>My Favorites:</h3>
+            <Playlist />
+        </div>
+    )
+}
+
+export default Favorites;
--- umlapp/src/components/SongInfo/songInfo.jsx
+++ umlapp/src/components/SongInfo/songInfo.jsx
@@ -11,18 +11,27 @@ const SongInfo = () => {
                 <h2>Title</h2>
                 <h2>Artist</h2>
                 <h2>Album</h2>
-                <h2 style={{float: 'right' }}>Artwork</h2>   
+                <h2 style={{textAlign: 'right' }}>Artwork</h2>   
             </div>
             <br />
             <div className="vtwrapper">
-                <span>
-                    <h4>Video:</h4>
-                    <YouTube />
-                </span>
-                <span>
-                    <h4>Tab:</h4>
-                    <Tab />
-                </span>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                        <span>
+                            <h4>Video:</h4>
+                            <YouTube />
+                        </span>
+                        </div>
+                        <div className="col-6">
+                        <span>
+                            <h4>Tab:</h4>
+                            <Tab />
+                        </span>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
