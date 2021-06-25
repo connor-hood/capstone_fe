9e15a80ca2d00f494e7dc20db7f88dcba417f833
connor-hood <connor_hood@yahoo.com>
2021-06-25 12:13:39 -0500

    got youtube and tab componenets into main song info

--- umlapp/src/components/SongInfo/songInfo.jsx
+++ umlapp/src/components/SongInfo/songInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import YouTube from '../YouTube/youtube';
 import Tab from '../Tab/tab';
+import './songInfo.css'
 
 
 const SongInfo = () => {
@@ -11,6 +12,17 @@ const SongInfo = () => {
                 <h2>Artist</h2>
                 <h2>Album</h2>
                 <h2 style={{float: 'right' }}>Artwork</h2>   
+            </div>
+            <br />
+            <div className="vtwrapper">
+                <span>
+                    <h4>Video:</h4>
+                    <YouTube />
+                </span>
+                <span>
+                    <h4>Tab:</h4>
+                    <Tab />
+                </span>
             </div>
         </div>
     )
